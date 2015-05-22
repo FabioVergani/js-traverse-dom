164a98bedce7cc224d214c6cdd3bbb16ab896e15
FabioVergani <fabio.vergani.1975@gmail.com>
2015-05-22 13:51:24 +0200

    Update tmp.js

--- tmp.js
+++ tmp.js
@@ -3,6 +3,43 @@ console.clear();
 (function(w){
 	"use strict";
 	function walk(node,filter){var f=filter,e=f(node);while(e){walk(e,f);e=e.nextSibling;}}
+
+	function tree(node){
+		var e=node, f=getProp;
+		f=f(tree.handler,f(e,'nodeType'));
+		if(f!==false){f(e);f=e.firstChild;}
+		return f;
+	}
+
+
+	var d=w.document,
+	isDefined=function(x){return typeof(x)!=='undefined';},
+	shortCall=function(x){return Function.prototype.call.bind(x);},
+	hasOwn=shortCall(Object.prototype.hasOwnProperty),
+	getProp=function(o,p){return (isDefined(o) && (p in o) && hasOwn(o,p))?o[p]:false;},//...
+	result=false;
+
+	//garbage:
+	shortCall=null;
+
+	//customize:
+	tree.handler=[
+		null,
+		function(tag){
+			var e=tag;console.dir(e.tagName);
+		},
+	];
+
+	walk(d.documentElement,tree);
+})(window);
+
+
+
+console.clear();
+//
+(function(w){
+	"use strict";
+	function walk(node,filter){var f=filter,e=f(node);while(e){walk(e,f);e=e.nextSibling;}}
 	function tree(node){var e=node;if(has(e,'nodeType')===1){tree.accepted(e);return e.firstChild;}}
 
 	var d=w.document,
